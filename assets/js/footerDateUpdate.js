543d6cfc5b4abf75358e3487278ee14b884bcf20
jpbadan <jpbadan@gmail.com>
2020-07-17 16:49:20 +0200

    Website first commit

---
+++ assets/js/footerDateUpdate.js
@@ -0,0 +1,5 @@
+var currentDate = new Date(); 
+
+let footer_text = document.getElementById("footer_text");
+footer_text.innerHTML = "&copy; " + currentDate.getFullYear() + 
+                        " João Pedro Badan, All rights reserved | Made with 🤍 in Toulouse";
